c6e2b35c9489c855d6cf00411dc87fb7c2bc4838
j k <janish.khanna233@gmail.com>
2019-05-24 12:31:17 -0500

    fixed some validations

--- Assignment2API/Models/Domain/BindingModel/PaymentBindingModel.cs
+++ Assignment2API/Models/Domain/BindingModel/PaymentBindingModel.cs
@@ -8,13 +8,15 @@ namespace Assignment2API.Models.Domain.BindingModel
 {
     public class PaymentBindingModel
     {
-        [Required]
+        [Required]        
         public decimal Amount { get; set; }
         [Required]
         public string NameOnCard { get; set; }
         [Required]
+        [CreditCard(ErrorMessage = "credit card not valid")]
         public string CreditCardNumber { get; set; }
         [Required]
+        [StringLength(4, ErrorMessage = "min length 3 and max 4", MinimumLength =3)]
         public string SecurityCode { get; set; }  
     }
 }
